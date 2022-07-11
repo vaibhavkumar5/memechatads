a3090882a90982b8ba512863349fc9b7448ed672
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2022-07-11 14:07:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./styles.css";
+import Logo from "./assets/2.png"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img id="1000345081_cpa_testing" src="https://ads.trafficjunky.net/tj_ads_pt?a=1000345081&member_id=1004325931&cb=
+    [RANDOM_NUMBER]&cti=[TRANSACTION_UNIQ_ID]&ctv=[VALUE_OF_THE_TRANSACTION]&ctd=[TRANSACTION_DESCRIPTION]"
+    width="1" height="1" border="0" style={{display:"none"}}  alt=''  />
+      <div className='container'>
+        <div className='row'>
+            <div className='col-1'>
+                <h3>Amy wants you to send <br /> these memes to her, join <br /> her and 3 million+ meme <br/> lovers
+                 </h3>
+                <button className='btn'> Download Now</button>
+            </div>
+            <div className='col-2'>
+              <img 
+              src={Logo} 
+              className="sideimg"
+              alt=' '
+              />
+              <div className='color-box'></div>
+              
+            </div>
+        </div>
+      </div>
+    </div> 
   );
 }
 
